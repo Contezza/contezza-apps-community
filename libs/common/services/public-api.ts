c8b617792818bd25ec6704362d9592b09c9ab168
wasyl <wasyl.lopatka@contezza.nl>
2023-03-10 13:50:06 +0100

    remove unused imports

--- libs/common/services/public-api.ts
+++ libs/common/services/public-api.ts
@@ -1,7 +1,5 @@
 export * from './common.service';
 export * from './destroy.service';
-export * from './lazy-dialog.service';
-export * from './mat-dialog.service';
 export * from './spinner-overlay.service';
 export * from './webscript.service';
 export * from './async-dialog.service';
