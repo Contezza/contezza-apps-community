35ee60dc608c946d9c586b512dabc1c17d930239
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-03-30 11:15:39 +0200

    [utils] add if check in resolve imports

--- libs/core/utils/src/lib/collections/object-utils.class.ts
+++ libs/core/utils/src/lib/collections/object-utils.class.ts
@@ -137,110 +137,114 @@ export class ContezzaObjectUtils {
             replace?: Replacer[];
         }
 
-        // resolve array element matchers into array indexes
-        keysWithImport.forEach((key) => {
-            const objectWithImports = getValue(object, key);
-            objectWithImports[importsKey] = ContezzaArrayUtils.asArray(objectWithImports[importsKey] as OrArray<string | Import>).map((importStringOrObject) => {
-                const importObject = typeof importStringOrObject === 'string' ? { id: importStringOrObject } : importStringOrObject;
-                let importId = importObject.id;
-
-                let important = false;
-                if (importId.endsWith(tagImportant)) {
-                    important = true;
-                    importId = importId.slice(0, -tagImportant.length);
-                }
-
-                let text = importId;
-                let replaced;
-
-                while (text !== replaced) {
-                    if (replaced) {
-                        text = replaced;
-                    }
-                    replaced = text.replace(/\[[^\[\]]*\]/, (match: string, offset: number, s: string) => {
-                        const subkey = s.substring(0, offset);
-                        const array = getValue(object, subkey);
-                        if (!Array.isArray(array)) {
-                            throw new Error('Object element with key ' + subkey + ' is expected to be an array.');
-                        }
-                        let index!: number;
-                        const slicedKey = match.slice(1, -1);
-                        try {
-                            index = ContezzaObjectUtils.findIndexMatch(JSON.parse(slicedKey), array);
-                        } catch (e) {
-                            index = array.findIndex((item) => item.id === slicedKey);
-                        }
-                        if (index === -1) {
-                            throw new Error('No matching import for key ' + s);
-                        }
-                        return '.' + index;
-                    });
-                }
-                if (!replaced) {
-                    replaced = text;
-                }
-
-                return { ...importObject, id: replaced + (important ? tagImportant : '') };
-            });
-        });
-
-        // make base import trees
-        const importTrees: Tree<{ id: string }, 'dependsOn', '*'>[] = keysWithImport.map((id) => ({
-            id,
-            dependsOn: ContezzaArrayUtils.asArray(getValue(object, id)[importsKey]).map((importObject) => ({
-                id: importObject.id.endsWith(tagImportant) ? importObject.id.slice(0, -tagImportant.length) : importObject.id,
-            })),
-        }));
-
-        // chain dependencies in import trees
-        importTrees.forEach((tree) => tree.dependsOn.forEach((dep) => (dep.dependsOn = importTrees.filter(({ id }) => id.startsWith(dep.id)))));
-
-        // detect circular dependencies
-        try {
-            JSON.stringify(importTrees);
-        } catch (e) {
-            throw new Error('Circular dependency detected while processing object imports.');
-        }
-
-        // follow the import trees to resolve the dependencies
-        const processedKeys = [];
-        const processImportObject = (importObject: Import) => {
-            // destructure to exclude id
-            const { id, ...imported } = getValue(object, importObject.id.endsWith(tagImportant) ? importObject.id.slice(0, -tagImportant.length) : importObject.id);
-            // deep copy to allow the imported object to be imported multiple times with different replacers
-            const copy = JSON.parse(JSON.stringify(imported));
-            const { replace, prefixIds } = importObject;
-
-            // replace
-            ContezzaObjectUtils.replace(copy, replace);
-
-            // prefix ids
-            if (prefixIds) {
-                const idKeys = ContezzaObjectUtils.findKeys(
-                    copy,
-                    (el) => typeof el === 'string',
-                    (subtarget) => Object.entries(subtarget).filter(([key, val]) => val && (key === 'id' || typeof val === 'object'))
-                );
-                idKeys.forEach((idKey) => ContezzaObjectUtils.setValue(copy, idKey, prefixIds + ContezzaObjectUtils.getValue(copy, idKey)));
-            }
-
-            return copy;
-        };
-        const recursion = (tree: Tree<{ id: string }, 'dependsOn', '*'>) => {
-            tree.dependsOn?.forEach((subtree) => recursion(subtree));
-            const key = tree.id;
-            if (keysWithImport.includes(key) && !processedKeys.includes(key)) {
+        if (keysWithImport.length) {
+            // resolve array element matchers into array indexes
+            keysWithImport.forEach((key) => {
                 const objectWithImports = getValue(object, key);
-                const [important, notImportant] = ContezzaArrayUtils.partition(objectWithImports[importsKey] as Import[], (importObject) => importObject.id.endsWith(tagImportant));
-                // merging order: not important imports < object self < important imports
-                // assign the result to the object self
-                Object.assign(objectWithImports, mergeObjects(...notImportant.map(processImportObject), objectWithImports, ...important.map(processImportObject)));
-                delete objectWithImports[importsKey];
+                objectWithImports[importsKey] = ContezzaArrayUtils.asArray(objectWithImports[importsKey] as OrArray<string | Import>).map((importStringOrObject) => {
+                    const importObject = typeof importStringOrObject === 'string' ? { id: importStringOrObject } : importStringOrObject;
+                    let importId = importObject.id;
 
-                processedKeys.push(key);
+                    let important = false;
+                    if (importId.endsWith(tagImportant)) {
+                        important = true;
+                        importId = importId.slice(0, -tagImportant.length);
+                    }
+
+                    let text = importId;
+                    let replaced;
+
+                    while (text !== replaced) {
+                        if (replaced) {
+                            text = replaced;
+                        }
+                        replaced = text.replace(/\[[^\[\]]*\]/, (match: string, offset: number, s: string) => {
+                            const subkey = s.substring(0, offset);
+                            const array = getValue(object, subkey);
+                            if (!Array.isArray(array)) {
+                                throw new Error('Object element with key ' + subkey + ' is expected to be an array.');
+                            }
+                            let index!: number;
+                            const slicedKey = match.slice(1, -1);
+                            try {
+                                index = ContezzaObjectUtils.findIndexMatch(JSON.parse(slicedKey), array);
+                            } catch (e) {
+                                index = array.findIndex((item) => item.id === slicedKey);
+                            }
+                            if (index === -1) {
+                                throw new Error('No matching import for key ' + s);
+                            }
+                            return '.' + index;
+                        });
+                    }
+                    if (!replaced) {
+                        replaced = text;
+                    }
+
+                    return { ...importObject, id: replaced + (important ? tagImportant : '') };
+                });
+            });
+
+            // make base import trees
+            const importTrees: Tree<{ id: string }, 'dependsOn', '*'>[] = keysWithImport.map((id) => ({
+                id,
+                dependsOn: ContezzaArrayUtils.asArray(getValue(object, id)[importsKey]).map((importObject) => ({
+                    id: importObject.id.endsWith(tagImportant) ? importObject.id.slice(0, -tagImportant.length) : importObject.id,
+                })),
+            }));
+
+            // chain dependencies in import trees
+            importTrees.forEach((tree) => tree.dependsOn.forEach((dep) => (dep.dependsOn = importTrees.filter(({ id }) => id.startsWith(dep.id)))));
+
+            // detect circular dependencies
+            try {
+                JSON.stringify(importTrees);
+            } catch (e) {
+                throw new Error('Circular dependency detected while processing object imports.');
             }
-        };
-        importTrees.forEach((tree) => recursion(tree));
+
+            // follow the import trees to resolve the dependencies
+            const processedKeys = [];
+            const processImportObject = (importObject: Import) => {
+                // destructure to exclude id
+                const { id, ...imported } = getValue(object, importObject.id.endsWith(tagImportant) ? importObject.id.slice(0, -tagImportant.length) : importObject.id);
+                // deep copy to allow the imported object to be imported multiple times with different replacers
+                const copy = JSON.parse(JSON.stringify(imported));
+                const { replace, prefixIds } = importObject;
+
+                // replace
+                ContezzaObjectUtils.replace(copy, replace);
+
+                // prefix ids
+                if (prefixIds) {
+                    const idKeys = ContezzaObjectUtils.findKeys(
+                        copy,
+                        (el) => typeof el === 'string',
+                        (subtarget) => Object.entries(subtarget).filter(([key, val]) => val && (key === 'id' || typeof val === 'object'))
+                    );
+                    idKeys.forEach((idKey) => ContezzaObjectUtils.setValue(copy, idKey, prefixIds + ContezzaObjectUtils.getValue(copy, idKey)));
+                }
+
+                return copy;
+            };
+            const recursion = (tree: Tree<{ id: string }, 'dependsOn', '*'>) => {
+                tree.dependsOn?.forEach((subtree) => recursion(subtree));
+                const key = tree.id;
+                if (keysWithImport.includes(key) && !processedKeys.includes(key)) {
+                    const objectWithImports = getValue(object, key);
+                    const [important, notImportant] = ContezzaArrayUtils.partition(objectWithImports[importsKey] as Import[], (importObject) =>
+                        importObject.id.endsWith(tagImportant)
+                    );
+                    // merging order: not important imports < object self < important imports
+                    // assign the result to the object self
+                    Object.assign(objectWithImports, mergeObjects(...notImportant.map(processImportObject), objectWithImports, ...important.map(processImportObject)));
+                    delete objectWithImports[importsKey];
+
+                    processedKeys.push(key);
+                }
+            };
+            importTrees.forEach((tree) => recursion(tree));
+        }
     }
 
     /**
