4c0148a1fc79f7099ec386ac8e51f73910e241e8
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-04-07 15:38:58 +0200

    [dynamicforms] format date-range-single-field

--- libs/dynamic-forms/src/lib/components/form-fields/date-range-single/date-range-single-field.component.ts
+++ libs/dynamic-forms/src/lib/components/form-fields/date-range-single/date-range-single-field.component.ts
@@ -3,29 +3,24 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
-import { DatetimeAdapter, MAT_DATETIME_FORMATS } from '@mat-datetimepicker/core';
-import { MAT_MOMENT_DATETIME_FORMATS, MomentDatetimeAdapter } from '@mat-datetimepicker/moment';
-
 import { Moment } from 'moment';
 
 import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 import { DestroyService } from '@contezza/core/services';
-import { DateRange } from '@contezza/core/utils';
+import { DATE_FORMATS, DateRange } from '@contezza/core/utils';
 
-import { DATE_FORMATS } from '../../../interfaces/date-formats.interface';
 import { ContezzaBaseFieldComponent } from '../base-field.component';
 
 @Component({
-    selector: 'contezza-date-tange-single-field',
+    selector: 'contezza-date-range-single-field',
     templateUrl: './date-range-single-field.component.html',
     styleUrls: ['./date-range-single-field.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [
+        // ideally we provide these in CommonModule, but Alfresco CoreModule injects NativeDateAdapter
         { provide: DateAdapter, useClass: MomentDateAdapter },
-        { provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS },
-        { provide: DatetimeAdapter, useClass: MomentDatetimeAdapter },
-        { provide: MAT_DATETIME_FORMATS, useValue: MAT_MOMENT_DATETIME_FORMATS },
+        { provide: MAT_DATE_FORMATS, useExisting: DATE_FORMATS },
     ],
 })
 export class DateRangeSingleFieldComponent extends ContezzaBaseFieldComponent<DateRange> implements OnInit {
