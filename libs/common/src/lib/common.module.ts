57da95de01991e59af53a3ec608307cb6e1e75d9
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-04-07 15:04:26 +0200

    [dynamicforms] date format configuration via injection token

--- libs/common/src/lib/common.module.ts
+++ libs/common/src/lib/common.module.ts
@@ -3,9 +3,26 @@ import { NgModule } from '@angular/core';
 import { TranslationService } from '@alfresco/adf-core';
 
 import { RouterStoreModule } from '@contezza/core/stores';
+import { DATE_FORMATS } from '@contezza/core/utils';
 
 @NgModule({
     imports: [RouterStoreModule],
+    providers: [
+        {
+            provide: DATE_FORMATS,
+            useValue: {
+                parse: {
+                    dateInput: 'l',
+                },
+                display: {
+                    dateInput: 'L',
+                    monthYearLabel: 'MMM YYYY',
+                    dateA11yLabel: 'LL',
+                    monthYearA11yLabel: 'MMMM YYYY',
+                },
+            },
+        },
+    ],
 })
 export class ContezzaCommonModule {
     constructor(translation: TranslationService) {
---
+++ libs/core/utils/src/lib/tokens/date-formats.token.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from '@angular/core';
+import { MatDateFormats } from '@angular/material/core';
+
+export const DATE_FORMATS = new InjectionToken<MatDateFormats>('date-formats');
