6a40574d2f87f57498e40df619ccf8a8159aa8c7
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-09-01 12:26:31 +0200

    [dynamic-forms] add popover in dialog

--- libs/dynamic-forms/shared/src/lib/models/dynamic-form-dialog-data.interface.ts
+++ libs/dynamic-forms/shared/src/lib/models/dynamic-form-dialog-data.interface.ts
@@ -1,15 +1,6 @@
+import { DialogData } from '@contezza/core/dialogs';
+
 import { ContezzaDynamicForm } from '../classes';
 import { ExtendedDynamicFormDefinition } from './dynamic-form-id';
 
-export interface DialogData {
-    title: string;
-    titleParams?: any;
-    buttons: DynamicFormDialogButtons;
-}
-
 export type DynamicFormDialogData = DialogData & { dynamicForm?: ContezzaDynamicForm; dynamicFormId?: ExtendedDynamicFormDefinition };
-
-export interface DynamicFormDialogButtons {
-    cancel: string;
-    submit: string;
-}
