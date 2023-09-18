56937956f3c6c1c77179f797edb0bd67076ac7ee
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-09-18 16:38:45 +0200

    [pipes] add docs

--- libs/core/pipes/src/lib/includes.pipe.ts
+++ libs/core/pipes/src/lib/includes.pipe.ts
@@ -2,6 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { DistributiveKeyof } from '@contezza/core/utils';
 
+/**
+ * Allows to use JS `in` operator in templates. Useful when working with TS union types.
+ */
 @Pipe({ standalone: true, name: 'includes' })
 export class IncludesPipe implements PipeTransform {
     transform<T, TKey extends DistributiveKeyof<T>>(value: T, key: TKey): value is T & { [K in TKey]: any } {
