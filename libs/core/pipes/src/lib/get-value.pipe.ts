b206e33ea8061aaaab47ee93aac68d883df23cc6
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-09-21 08:29:42 +0200

    [pipes] fix input

--- libs/core/pipes/src/lib/get-value.pipe.ts
+++ libs/core/pipes/src/lib/get-value.pipe.ts
@@ -7,7 +7,7 @@ import { KeyOf, ObjectUtils, TypeOf } from '@contezza/core/utils';
  */
 @Pipe({ standalone: true, name: 'getValue' })
 export class GetValuePipe implements PipeTransform {
-    transform<T, TKey extends KeyOf<T> & (string | number)[]>(target: T, ...keys: TKey): TypeOf<T, TKey> {
+    transform<T, TKey extends KeyOf<T> & (string | number)[]>(target: T, keys: TKey): TypeOf<T, TKey> {
         // @ts-ignore
         // otherwise TS2321: Excessive stack depth comparing types...
         return ObjectUtils.getValue(target, ...keys);
