9c9e7180fd783ebad51950ccb62b22e760a4fb20
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-10-12 09:42:22 +0200

    [utils] refactor jwt - add docs

--- libs/core/utils/src/lib/classes/jwt.ts
+++ libs/core/utils/src/lib/classes/jwt.ts
@@ -1,20 +1,45 @@
-import { JwtUtils } from '../collections';
-
-export class Jwt {
-    private _parsedToken: any;
+/**
+ * Implements a JWT (JSON Web Token). Supports decoding and validation.
+ */
+export class Jwt<T = any> {
+    private _parsedToken: T & { exp: number };
 
     constructor(private readonly _token: string) {}
 
-    parse(): any {
-        return (this._parsedToken ??= JwtUtils.parse(this._token));
+    decode(): T & { exp: number } {
+        return (this._parsedToken ??= Jwt.decode(this._token));
     }
 
+    /**
+     * Validates the token against its expiration date.
+     */
     isValid(): boolean {
-        const { exp } = this.parse();
-        return Date.now() < exp * 1000;
+        return !this.isExpired();
     }
 
+    /**
+     * Checks whether the token expiration date has already passed.
+     */
     isExpired(): boolean {
-        return !this.isValid();
+        const { exp } = this.decode();
+        return Date.now() >= exp * 1000;
+    }
+
+    /**
+     * Decodes a JWT token.
+     *
+     * @param token A JWT token
+     * @returns The decoded token
+     */
+    static decode<T>(token: string): T & { exp: number } {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split('')
+                .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('')
+        );
+        return JSON.parse(jsonPayload);
     }
 }
