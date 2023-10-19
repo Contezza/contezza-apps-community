ea9042db05f926c2983e209d0dfa1651defbc002
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-10-19 14:30:21 +0200

    [dynamic-forms] make input standalone

--- libs/dynamic-forms/src/lib/components/form-fields/input/input-field.module.ts
+++ libs/dynamic-forms/src/lib/components/form-fields/input/input-field.module.ts
@@ -1,25 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { TranslateModule } from '@ngx-translate/core';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 
 import { ContezzaDynamicFormExtensionService } from '@contezza/dynamic-forms/shared';
 
-import { InputMaskModule } from '@ngneat/input-mask';
-
 import { InputFieldComponent } from './input-field.component';
-import { ContezzaDynamicFormsCommonModule } from '../../../dynamic-forms.common.module';
 
-@NgModule({
-    imports: [CommonModule, ContezzaDynamicFormsCommonModule, InputMaskModule.forRoot(), MatButtonModule, MatIconModule, MatInputModule, ReactiveFormsModule, TranslateModule],
-    declarations: [InputFieldComponent],
-    exports: [InputFieldComponent],
-})
+@NgModule({})
 export class ContezzaInputFieldModule {
     constructor(extensions: ContezzaDynamicFormExtensionService) {
         extensions.setFieldComponents({
