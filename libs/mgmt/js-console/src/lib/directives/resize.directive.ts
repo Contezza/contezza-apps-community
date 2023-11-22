a2d4673a28aa7e41a1c51cd5e1568d78a1094424
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2023-11-22 13:10:06 +0100

    [js-console] make resizer generic

--- libs/mgmt/js-console/src/lib/directives/resize.directive.ts
+++ libs/mgmt/js-console/src/lib/directives/resize.directive.ts
@@ -1,52 +1,95 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+type Direction = 'ns' | 'ew';
 
 @Directive({
     standalone: true,
     selector: '[jsConsoleResize]',
 })
-export class JsConsoleResizeDirective {
-    grabber = false;
-    heigth: number;
+export class JsConsoleResizeDirective implements AfterViewInit {
+    private resizing = false;
 
-    // eslint-disable-next-line @angular-eslint/no-input-rename
-    @Input('topResize')
-    topElement: HTMLElement;
+    private readonly config: Record<Direction, { cursor: string }> = {
+        ns: {
+            cursor: 'ns-resize',
+        },
+        ew: {
+            cursor: 'ew-resize',
+        },
+    };
 
-    // eslint-disable-next-line @angular-eslint/no-input-rename
-    @Input('bottomResize')
-    bottomElement: HTMLElement;
+    direction: Direction;
+    firstElement: HTMLElement;
+    secondElement: HTMLElement;
 
-    constructor(private el: ElementRef<HTMLElement>) {}
+    @HostBinding('style.cursor')
+    cursor: string;
 
-    @HostListener('window:resize', ['$event']) onResize(event) {
-        this.heigth = event.target.outerHeight;
+    constructor(private readonly el: ElementRef<HTMLElement>) {}
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            // setup based on html structure
+            // conditions: (1) parent is a flex container (2) parent has exactly three children (3) the second children is this directive's host
+            const parentElement = this.el.nativeElement.parentElement;
+            const parentStyle = getComputedStyle(parentElement);
+            if (parentStyle.display === 'flex' && parentElement.children.length === 3 && parentElement.children.item(1) === this.el.nativeElement) {
+                // resize direction based on the parent flex-direction
+                this.direction = parentStyle.flexDirection === 'column' ? 'ns' : 'ew';
+                this.cursor = this.config[this.direction].cursor;
+                // resized elements are the siblings of this directive's host
+                this.firstElement = parentElement.children.item(0) as HTMLElement;
+                this.secondElement = parentElement.children.item(2) as HTMLElement;
+            } else {
+                // warn if conditions are not satisfied
+                console.warn('Directive `jsConsoleResize` cannot be applied.');
+            }
+        }, 10);
     }
+
     @HostListener('mousedown') onMouseDown() {
-        this.grabber = true;
-        this.el.nativeElement.classList.add('side-panel');
-        document.body.style.cursor = 'ns-resize;';
+        this.resizing = true;
+        this.el.nativeElement.classList.add('resizing');
+        document.body.style.cursor = this.cursor;
     }
 
     @HostListener('window:mouseup') onMouseUp() {
-        this.grabber = false;
-        this.el.nativeElement.classList.remove('side-panel');
+        this.resizing = false;
+        this.el.nativeElement.classList.remove('resizing');
         document.body.style.cursor = 'default';
     }
 
     @HostListener('window:mousemove', ['$event']) onMouseMove(event: MouseEvent) {
-        if (this.grabber) {
+        if (this.resizing) {
             event.preventDefault();
 
-            const containerSize = this.el.nativeElement.parentElement.scrollHeight;
-            const offset = window.scrollY + this.el.nativeElement.parentElement.getBoundingClientRect().top;
+            // resizing works by changing the flex-basis of the resized elements
+            // computation is based on X or Y coordinates, depending on the direction
 
-            if (event.movementY > 0) {
-                this.bottomElement.style.flex = `0 5 ${offset + containerSize - event.clientY}px`;
-                this.topElement.style.flex = `1 5 ${event.clientY - offset}px`;
-            } else {
-                this.topElement.style.flex = `0 5 ${event.clientY - offset}px`;
-                this.bottomElement.style.flex = `1 5 ${offset + containerSize - event.clientY}px`;
+            let mouseCoordinate: number;
+            let containerSize: number;
+            let containerOffset: number;
+
+            switch (this.direction) {
+                case 'ns': {
+                    // based on Y coordinates
+                    mouseCoordinate = event.clientY;
+                    containerSize = this.el.nativeElement.parentElement.scrollHeight;
+                    containerOffset = window.scrollY + this.el.nativeElement.parentElement.getBoundingClientRect().top;
+                    break;
+                }
+                case 'ew': {
+                    // based on X coordinates
+                    mouseCoordinate = event.clientX;
+                    containerSize = this.el.nativeElement.parentElement.scrollWidth;
+                    containerOffset = window.scrollX + this.el.nativeElement.parentElement.getBoundingClientRect().left;
+                    break;
+                }
             }
+
+            const firstElementSize = mouseCoordinate - containerOffset;
+            this.firstElement.style.flex = `1 5 ${firstElementSize}px`;
+            this.secondElement.style.flex = `0 5 ${containerSize - firstElementSize}px`;
         }
     }
 }
