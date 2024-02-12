83677cb7c41b4f8dfce88730f432b77281e36d70
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2024-02-12 08:52:51 +0100

    [dynamic-forms] add dialog return type

--- libs/dynamic-forms/dialog/src/lib/dynamic-form-dialog.service.ts
+++ libs/dynamic-forms/dialog/src/lib/dynamic-form-dialog.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { MatDialogConfig } from '@angular/material/dialog';
+
+import { Observable } from 'rxjs';
 
 import { DialogComponentService } from '@contezza/core/dialogs';
 
@@ -8,5 +11,9 @@ import { DynamicFormDialogData } from '@contezza/dynamic-forms/shared';
 export class DynamicFormDialogService<ReturnType = any> extends DialogComponentService<{ data: DynamicFormDialogData }, DynamicFormDialogData, ReturnType> {
     protected get module() {
         return import('@contezza/dynamic-forms').then((lib) => ({ module: { getComponent: () => lib.DynamicFormDialogComponent } }));
+    }
+
+    open<TResponse = ReturnType>(data: MatDialogConfig<DynamicFormDialogData>): Observable<TResponse | undefined> {
+        return super.open(data) as any;
     }
 }
