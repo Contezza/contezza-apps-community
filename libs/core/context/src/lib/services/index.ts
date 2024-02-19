c70e40ff64d13d4df39854189f9291fe7c3125e7
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2024-02-19 08:01:20 +0100

    [context] fix index

--- libs/core/context/src/lib/services/index.ts
+++ libs/core/context/src/lib/services/index.ts
@@ -1,3 +1,4 @@
+export * from './action-payload.service';
 export * from './actions.service';
 export * from './component-store.service';
 export * from './rule-context.service';
--- libs/core/context/src/lib/services/public-api.ts
+++
@@ -1,4 +0,0 @@
-export * from './action-payload.service';
-export * from './actions.service';
-export * from './component-store.service';
-export * from './rule-context.service';
