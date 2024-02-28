ad2731a9ea130300f41c617229b46f04ab615f79
mbommerson <maaike.bommerson@contezza.nl>
2024-02-28 15:58:25 +0100

    [js-console] executionResult (#27113)

--- libs/mgmt/js-console/src/lib/services/console.service.ts
+++ libs/mgmt/js-console/src/lib/services/console.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { catchError, map, take } from 'rxjs/operators';
+import {BehaviorSubject, delay, Observable, of, Subject} from 'rxjs';
+import {catchError, map, switchMap, take, takeUntil} from 'rxjs/operators';
 
 import { NodesApiService } from '@alfresco/adf-content-services';
 
@@ -11,6 +11,7 @@ import { WebscriptService } from '@contezza/core/services';
 
 import { setScriptExecutionTime } from '../store/actions';
 import { ConsoleScript, ExecuteConsolePayload, ExecuteConsoleResponse } from '../interfaces/js-console';
+import {ContezzaObservables} from "@contezza/core/utils";
 
 @Injectable({
     providedIn: 'root',
@@ -18,14 +19,31 @@ import { ConsoleScript, ExecuteConsolePayload, ExecuteConsoleResponse } from '..
 export class JsConsoleService {
     private readonly EXECUTE_URL = 'de/fme/jsconsole/execute';
     private readonly LIST_SCRIPTS_URL = 'de/fme/jsconsole/listscripts';
+    private readonly EXECUTION_RESULT_URL = 'de/fme/jsconsole/${resultChannel}/executionResult'
+    results = new Subject<string[]>()
 
     constructor(private readonly webscript: WebscriptService, private readonly nodesApiService: NodesApiService, private readonly store: Store<unknown>) {}
-
     executeScript(payload: ExecuteConsolePayload): Observable<ExecuteConsoleResponse> {
         const startTime = new Date();
-
+        console.log(startTime.getTime())
+        // console.log(payload)
+        payload.resultChannel = "" + startTime.getTime()
+        const stop$ = new Subject<void>()
+        console.log(this.results)
+// results meegeven als de output tot de sript klaar is???
+        this.results.subscribe((val) => console.log(val))
+        ContezzaObservables.while(() => true, () => of(void 0).pipe(
+            delay(2000),
+            switchMap(() => this.webscript.get(
+                this.EXECUTION_RESULT_URL.replace('${resultChannel}', payload.resultChannel)))),
+            (val: ExecuteConsoleResponse) => this.results.next(val.printOutput))
+            .pipe(
+                takeUntil(stop$))
+                .subscribe()
         return this.webscript.post(this.EXECUTE_URL, payload).pipe(
             map((response: ExecuteConsoleResponse) => {
+                stop$.next()
+                stop$.complete()
                 this.store.dispatch(
                     setScriptExecutionTime({
                         executionTime: {
@@ -39,7 +57,8 @@ export class JsConsoleService {
             catchError((error) => {
                 const parsedError = JSON.parse(JSON.stringify(error));
                 const errorBody = parsedError.response.body;
-
+                stop$.next()
+                stop$.complete()
                 return of({
                     error: {
                         statusCode: errorBody.status.code,
