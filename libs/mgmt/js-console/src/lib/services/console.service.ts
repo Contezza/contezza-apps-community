b306c2ff8bcaa8e69a15727fb13ada37c3a0c7c9
mbommerson <maaike.bommerson@contezza.nl>
2024-03-01 13:13:18 +0100

    [js-console] output weergeven (#27113)

--- libs/mgmt/js-console/src/lib/services/console.service.ts
+++ libs/mgmt/js-console/src/lib/services/console.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 
-import {BehaviorSubject, delay, Observable, of, Subject} from 'rxjs';
-import {catchError, map, switchMap, take, takeUntil} from 'rxjs/operators';
+import { BehaviorSubject, delay, Observable, of, Subject } from 'rxjs';
+import { catchError, map, switchMap, take, takeUntil } from 'rxjs/operators';
 
 import { NodesApiService } from '@alfresco/adf-content-services';
 
@@ -11,7 +11,7 @@ import { WebscriptService } from '@contezza/core/services';
 
 import { setScriptExecutionTime } from '../store/actions';
 import { ConsoleScript, ExecuteConsolePayload, ExecuteConsoleResponse } from '../interfaces/js-console';
-import {ContezzaObservables} from "@contezza/core/utils";
+import { ContezzaObservables } from '@contezza/core/utils';
 
 @Injectable({
     providedIn: 'root',
@@ -19,56 +19,63 @@ import {ContezzaObservables} from "@contezza/core/utils";
 export class JsConsoleService {
     private readonly EXECUTE_URL = 'de/fme/jsconsole/execute';
     private readonly LIST_SCRIPTS_URL = 'de/fme/jsconsole/listscripts';
-    private readonly EXECUTION_RESULT_URL = 'de/fme/jsconsole/${resultChannel}/executionResult'
-    results = new Subject<string[]>()
+    private readonly EXECUTION_RESULT_URL = 'de/fme/jsconsole/${resultChannel}/executionResult';
 
     constructor(private readonly webscript: WebscriptService, private readonly nodesApiService: NodesApiService, private readonly store: Store<unknown>) {}
     executeScript(payload: ExecuteConsolePayload): Observable<ExecuteConsoleResponse> {
         const startTime = new Date();
-        console.log(startTime.getTime())
-        // console.log(payload)
-        payload.resultChannel = "" + startTime.getTime()
-        const stop$ = new Subject<void>()
-        console.log(this.results)
-// results meegeven als de output tot de sript klaar is???
-        this.results.subscribe((val) => console.log(val))
-        ContezzaObservables.while(() => true, () => of(void 0).pipe(
-            delay(2000),
-            switchMap(() => this.webscript.get(
-                this.EXECUTION_RESULT_URL.replace('${resultChannel}', payload.resultChannel)))),
-            (val: ExecuteConsoleResponse) => this.results.next(val.printOutput))
+        payload.resultChannel = '' + startTime.getTime();
+        const stop$ = new Subject<void>();
+        const results$ = new Subject<ExecuteConsoleResponse>();
+
+        // er wordt 1 observable gebruikt die door de while gehaald wordt, tot de execute complete is.
+        ContezzaObservables.while(
+            () => true,
+            () =>
+                of(void 0).pipe(
+                    delay(1000),
+                    switchMap(() => this.webscript.get(this.EXECUTION_RESULT_URL.replace('${resultChannel}', payload.resultChannel)))
+                ),
+            (val: ExecuteConsoleResponse) => results$.next(val)
+        )
+            .pipe(takeUntil(stop$))
+            .subscribe();
+        this.webscript
+            .post(this.EXECUTE_URL, payload)
             .pipe(
-                takeUntil(stop$))
-                .subscribe()
-        return this.webscript.post(this.EXECUTE_URL, payload).pipe(
-            map((response: ExecuteConsoleResponse) => {
-                stop$.next()
-                stop$.complete()
-                this.store.dispatch(
-                    setScriptExecutionTime({
-                        executionTime: {
-                            ms: new Date().getTime() - startTime.getTime(),
-                            timestamp: startTime,
+                map((response: ExecuteConsoleResponse) => {
+                    stop$.next();
+                    stop$.complete();
+                    this.store.dispatch(
+                        setScriptExecutionTime({
+                            executionTime: {
+                                ms: new Date().getTime() - startTime.getTime(),
+                                timestamp: startTime,
+                            },
+                        })
+                    );
+                    return { ...response, error: undefined };
+                }),
+                catchError((error) => {
+                    const parsedError = JSON.parse(JSON.stringify(error));
+                    const errorBody = parsedError.response.body;
+                    stop$.next();
+                    stop$.complete();
+                    return of({
+                        error: {
+                            statusCode: errorBody.status.code,
+                            statusText: errorBody.status.name,
+                            callstack: errorBody.callstack,
+                            message: errorBody.message,
                         },
-                    })
-                );
-                return { ...response, error: undefined };
-            }),
-            catchError((error) => {
-                const parsedError = JSON.parse(JSON.stringify(error));
-                const errorBody = parsedError.response.body;
-                stop$.next()
-                stop$.complete()
-                return of({
-                    error: {
-                        statusCode: errorBody.status.code,
-                        statusText: errorBody.status.name,
-                        callstack: errorBody.callstack,
-                        message: errorBody.message,
-                    },
-                });
-            })
-        );
+                    });
+                })
+            )
+            .subscribe((response) => {
+                results$.next(response);
+                results$.complete();
+            });
+        return results$;
     }
 
     getScriptsList(): Observable<Array<ConsoleScript>> {
