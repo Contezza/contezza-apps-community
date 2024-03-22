df47fbf84992d1fedcf3a9c35a44e8224f303fdc
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2024-03-22 14:53:22 +0100

    [utils] fix provide api util

--- libs/core/services/src/index.ts
+++ libs/core/services/src/index.ts
@@ -1,6 +1,1 @@
-export * from './lib/async-dialog.service';
-export * from './lib/destroy.service';
-export * from './lib/is-small-screen.service';
-export * from './lib/spinner-overlay.service';
-export * from './lib/webscript.service';
-export * from './lib/preferences.service';
+export * from './lib';
