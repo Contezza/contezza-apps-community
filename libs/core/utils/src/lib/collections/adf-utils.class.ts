00118b11a3a9620cbc41f9e25e626584c7eb4b93
diego.mirandola@contezza.nl <diego.mirandola@contezza.nl>
2024-03-26 08:10:19 +0100

    [utils] move toNoderef to alfresco utils

--- libs/core/utils/src/lib/collections/adf-utils.class.ts
+++ libs/core/utils/src/lib/collections/adf-utils.class.ts
@@ -60,17 +60,6 @@ export class AdfUtils {
             action.children.forEach((child) => ContezzaAdfUtils.setActionDefaults(child));
         }
     }
-
-    /**
-     * Turns the given id into a noderef by prefixing it with `workspace://SpacesStore/`.
-     * If the given parameter already is a noderef then it is immediately returned.
-     * N.B.: the parameter is assumed to be either an uuid or a noderef, no check is performed.
-     *
-     * @param id
-     */
-    static toNoderef(id: string): string {
-        return id.includes('/') ? AdfUtils.prefixSpacesStore + id : id;
-    }
 }
 
 /**
---
+++ libs/core/utils/src/lib/collections/alfresco.utils.ts
@@ -0,0 +1,14 @@
+export class AlfrescoUtils {
+    static readonly prefixSpacesStore = 'workspace://SpacesStore/';
+
+    /**
+     * Turns the given id into a noderef by prefixing it with `workspace://SpacesStore/`.
+     * If the given parameter already is a noderef then it is immediately returned.
+     * N.B.: the parameter is assumed to be either an uuid or a noderef, no check is performed.
+     *
+     * @param id
+     */
+    static toNoderef(id: string): string {
+        return id.includes('/') ? AlfrescoUtils.prefixSpacesStore + id : id;
+    }
+}
