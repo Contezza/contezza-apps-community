5d2738b83bb4b32fd56af59b9fac048675631a14
Sabine Schoppers <sabine.schoppers@contezza.nl>
2024-07-30 15:15:52 +0200

    clean up app.config.js

--- apps/demo-app/config/app.config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    modules: [],
-    extensionJsons: ['gemams/contezza-search.gemams.navbar.json'],
-    jsConsole: { service: 'ootb' },
-};
--- apps/demo-app/config/app.config.prod.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    modules: [],
-    extensionJsons: [],
-    jsConsole: { service: 'ootb' },
-};
