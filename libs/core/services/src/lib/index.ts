30d8fd5c47050c779ab02f34218ae0d31fbb8ff4
Diego Mirandola <diego.mirandola@contezza.nl>
2025-03-07 15:27:52 +0100

    [core] move ContezzaCommonService refresh (#31141)

--- libs/core/services/src/lib/index.ts
+++ libs/core/services/src/lib/index.ts
@@ -3,5 +3,6 @@ export * from './destroy.service';
 export * from './is-small-screen.service';
 export * from './preferences.service';
 export * from './provide-webscript-api-service';
+export * from './refresh.subject';
 export * from './spinner-overlay.service';
 export * from './webscript.service';
