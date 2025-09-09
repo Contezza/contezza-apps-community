e7860edd5a5e0cd45fdc5f827c65da2aacfd5ecc
Diego Mirandola <diego.mirandola@contezza.nl>
2025-09-09 15:27:37 +0200

    [content-services] block thumbnail refresh when searching (#34678)

--- libs/content-services/src/lib/components/columns/thumbnail/thumbnail.column.component.ts
+++ libs/content-services/src/lib/components/columns/thumbnail/thumbnail.column.component.ts
@@ -7,11 +7,12 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { TranslateModule } from '@ngx-translate/core';
 
-import { Observable, of, switchMap } from 'rxjs';
+import { debounceTime, filter, map, Observable, of, switchMap, withLatestFrom } from 'rxjs';
 
 import { RuleContextService, SelectionStore } from '@contezza/core/context';
 import { IconDirective } from '@contezza/core/directives';
 import { ColumnComponent, ThumbnailConfig, ThumbnailData, ThumbnailService } from '@contezza/content-services/shared';
+import { SearchResultsService } from '@contezza/content-services/search/shared';
 
 @Component({
     standalone: true,
@@ -29,12 +30,24 @@ export class ThumbnailColumnComponent<TItem> extends ColumnComponent<TItem, Thum
 
     data$: Observable<ThumbnailData>;
 
-    constructor(private readonly ruleContext$: RuleContextService, private readonly thumbnails: ThumbnailService, @Optional() private readonly selection?: SelectionStore<TItem>) {
+    constructor(
+        private readonly ruleContext$: RuleContextService,
+        private readonly thumbnails: ThumbnailService,
+        @Optional() private readonly selection?: SelectionStore<TItem>,
+        @Optional() private readonly searchResultsService?: SearchResultsService
+    ) {
         super();
     }
 
     ngOnInit() {
         this.isSelected$ = this.selection?.selected$(this.item) || of(false);
-        this.data$ = this.ruleContext$.pipe(switchMap((context) => this.thumbnails.getThumbnailData(this.item, this.column.data, context)));
+        this.data$ = this.ruleContext$.pipe(
+            debounceTime(0),
+            // block any update while searching, otherwise the thumbnail could display incorrect information due to mixed context
+            withLatestFrom(this.searchResultsService?.searching$ || of(false)),
+            filter(([, searching]) => !searching),
+            map(([context]) => context),
+            switchMap((context) => this.thumbnails.getThumbnailData(this.item, this.column.data, context))
+        );
     }
 }
