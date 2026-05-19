0f5df15dcccc5e7cbf056f556acb7d77cff97b96
Diego Mirandola <diego.mirandola@contezza.nl>
2026-05-19 10:48:47 +0200

    [content-services] improve channel model (#37386)

--- libs/content-services/share/shared/src/lib/models/channel.ts
+++ libs/content-services/share/shared/src/lib/models/channel.ts
@@ -1,8 +1,6 @@
 export interface Channel {
     id: string;
     actionType: string;
-    label?: string;
-    tooltip?: string;
-    description?: string;
+    label: string;
     allowedByLinkTypes?: string[];
 }
