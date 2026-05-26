a25e9aaacd10b339ea878e28699012d16c368299
Diego Mirandola <diego.mirandola@contezza.nl>
2026-05-26 14:05:06 +0200

    [layout] keyboard control in collapsed navbar (#36855)

--- libs/layout/components/navbar/src/lib/components/simple-item/simple-item.component.ts
+++ libs/layout/components/navbar/src/lib/components/simple-item/simple-item.component.ts
@@ -1,9 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 import { IconDirective } from '@contezza/core/directives';
 
@@ -11,18 +10,19 @@ import { ItemComponent } from '../item.component';
 
 @Component({
     standalone: true,
-    imports: [CommonModule, MatButtonModule, MatIconModule, TranslateModule, IconDirective],
+    imports: [MatButtonModule, MatIconModule, TranslatePipe, IconDirective],
     selector: 'contezza-navbar-simple-item',
     templateUrl: './simple-item.component.html',
     styleUrls: ['./simple-item.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
     host: { class: 'contezza-navbar-simple-item' },
 })
 export class SimpleItemComponent extends ItemComponent {
     onClick() {
-        this.navigateTo(this.item);
+        this.navigateTo(this.item());
     }
 
     protected get active(): boolean {
-        return this.isActive(this.item);
+        return this.isActive(this.item());
     }
 }
